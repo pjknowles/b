c6f0ed142d6109fef07b087bc760a2e4f497dec9
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2019-06-16 21:04:09 +0100

    .

---
+++ library.cpp
@@ -0,0 +1,7 @@
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+  std::cout << "from library" << std::endl;
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef B_LIBRARY_H
+#define B_LIBRARY_H
+
+void hello();
+
+#endif //B_LIBRARY_H
